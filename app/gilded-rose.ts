b0b46c2f6a4c5166475d7ce476b6a6193a774b29
Anca Circioroaba <Anca.Circioroaba@softwire.com>
2023-06-28 15:37:55 +0300

    GlidedRose done

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,48 +19,63 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
+            // folosim o variabila pt this.items[i] ca sa fie mai usor de citit
+            const currentItem = this.items[i];
+            // retinem in urmatoarele 3 variabile daca item-ul curent este cumva din cele 3 exceptii
+            const isAgedBrie: Boolean = currentItem.name === 'Aged Brie';
+            const isBackstagePass : Boolean = currentItem.name === 'Backstage passes to a TAFKAL80ETC concert';
+            const isSulfuras : Boolean = currentItem.name === 'Sulfuras, Hand of Ragnaros';
+            const isConjured : Boolean = currentItem.name === 'Conjured Mana Cake';
+            // daca este Aged Brie
+            if (isAgedBrie) {
+                if ( currentItem.quality < 50) {
+                    currentItem.quality++;
+                }
+            } else if (isBackstagePass) {
+                //daca este Backstage Passes
+                if ( currentItem.quality < 50) {
+                    currentItem.quality++;
+                    if (currentItem.sellIn < 11 &&  currentItem.quality < 50) {
+                        currentItem.quality++;
+                    }
+                    if (currentItem.sellIn < 6 &&  currentItem.quality< 50) {
+                        currentItem.quality++;
                     }
                 }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
+                // daca se duce termenul de vanzare , quality se face 0
+                if (currentItem.sellIn < 0) {
+                    currentItem.quality = 0;
+                }
+            } else if (isConjured) {
+                // daca este Conjured, scadem quality de 2 ori daca nu devine negativ
+                if ( currentItem.quality > 0) {
+                    currentItem.quality--;
+                }
+                if (currentItem.quality > 0) {
+                    currentItem.quality--;
+                }
+                // daca Sellin este negativ, atunci scade "twice as fast", adica mai scadem de 2 ori
+                if(currentItem.sellIn < 0) {
+                    if ( currentItem.quality > 0) {
+                        currentItem.quality--;
                     }
+                    if ( currentItem.quality > 0) {
+                        currentItem.quality--;
+                    }
+                }
+            } else if (!isSulfuras) {
+                //daca este element normal, scadem o data
+                if ( currentItem.quality > 0) {
+                    currentItem.quality--;
+                }
+                // daca a expirat sellIn, mai scadem o data quality
+                if (currentItem.sellIn < 0 &&  currentItem.quality > 0) {
+                    currentItem.quality--;
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
+
+            if (!isSulfuras) {
+                currentItem.sellIn--;
             }
         }
 
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,3 +1,28 @@
+import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-// Add a master test here
+describe('Master test', function () {
+
+    it('should', function() {
+        //given
+        const item1 : Item = new Item('Aged Brie', 20,25);
+        const item2 : Item = new Item('Phone', 0, 24);
+        const item3: Item = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 7);
+        const item4: Item = new Item('In piata la Obor', -1, 13);
+        const item5: Item = new Item('Conjured Mana Cake', 3, 19);
+        const item6: Item = new Item('Conjured Mana Cake', 4, 1);
+        const item7: Item = new Item('Conjured Mana Cake', -1, 15);
+        const gildedRose : GildedRose = new GildedRose([ item1, item2, item3, item4, item5, item6, item7]);
+        const items : Item[] = gildedRose.updateQuality();
+        const correct1: Item = new Item('Aged Brie', 19, 26);
+        const correct2 : Item = new Item('Phone', -1, 23);
+        const correct3: Item = new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10);
+        const correct4: Item = new Item('In piata la Obor', -2, 11);
+        const correct5: Item = new Item('Conjured Mana Cake', 2, 17);
+        const correct6: Item = new Item('Conjured Mana Cake', 3, 0);
+        const correct7 :Item = new Item('Conjured Mana Cake', -2, 11);
+        const correctitems : Item[] = [correct1, correct2, correct3,  correct4, correct5, correct6, correct7];
+        expect(items).deep.equal(correctitems);
+    });
+
+});
